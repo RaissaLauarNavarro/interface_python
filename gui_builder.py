fef9467204cd27f294de19bc5867da938db45932
Raissa Lauar Navarro <raissalgns@gmail.com>
2025-08-15 12:25:38 -0300

    add convert button

--- gui_builder.py
+++ gui_builder.py
@@ -52,43 +52,48 @@ class GUIBuilder:
         app.main_frame = ctk.CTkFrame(app, fg_color="transparent")
         app.main_frame.grid(row=0, column=1, padx=(10, 20), pady=20, sticky="nsew")
         app.main_frame.grid_columnconfigure(0, weight=1)
-        app.main_frame.grid_rowconfigure(2, weight=1)
+        app.main_frame.grid_rowconfigure(0, weight=0)
+        app.main_frame.grid_rowconfigure(1, weight=0)
+        app.main_frame.grid_rowconfigure(2, weight=0)
+        app.main_frame.grid_rowconfigure(3, weight=1)
         
         # Os botões agora estão um abaixo do outro.
         app.btn_execute = ctk.CTkButton(app.main_frame, text="✨ Dividir Imagens", height=50, font=ctk.CTkFont(size=16, weight="bold"), command=app._handle_split_image, fg_color=app.COLOR_PRIMARY_BUTTON, hover_color=app.COLOR_PRIMARY_HOVER)
         app.btn_execute.grid(row=0, column=0, padx=0, pady=(0, 5), sticky="ew")
 
-        ctk.CTkButton(app.main_frame, text="🎨 Criar Paleta de Cores", height=50, font=ctk.CTkFont(size=16, weight="bold"), command=app._handle_create_palette_button, fg_color=app.COLOR_PRIMARY_BUTTON2, hover_color=app.COLOR_PRIMARY_HOVER).grid(row=1, column=0, padx=0, pady=(0, 10), sticky="ew")
+        ctk.CTkButton(app.main_frame, text="🎨 Criar Paleta de Cores", height=50, font=ctk.CTkFont(size=16, weight="bold"), command=app._handle_create_palette_button, fg_color=app.COLOR_PRIMARY_BUTTON2, hover_color=app.COLOR_PRIMARY_HOVER).grid(row=1, column=0, padx=0, pady=(2), sticky="ew")
         
+        ctk.CTkButton(app.main_frame, text="↪️ Converter formato", height=50, font=ctk.CTkFont(size=16, weight="bold"), command=app._handle_create_palette_button, fg_color=app.COLOR_PRIMARY_BUTTON2, hover_color=app.COLOR_PRIMARY_HOVER).grid(row=2, column=0, padx=0, pady=(2), sticky="ew")
+
         # Cria a tabview antes de adicionar as abas
         app.tabview = ctk.CTkTabview(app.main_frame, fg_color=app.COLOR_FRAME, segmented_button_selected_color=app.COLOR_PRIMARY_BUTTON, segmented_button_selected_hover_color=app.COLOR_PRIMARY_HOVER)
-        app.tabview.grid(row=2, column=0, columnspan=2, sticky="nsew")
+        app.tabview.grid(row=3, column=0, columnspan=2, sticky="nsew")
         app.tabview.add("Log de Atividades")
         app.tabview.add("Preview com Grid")
         app.tabview.add("Gerador de Paleta de Cores")
 
         # Aba de Preview com Grid
         app.preview_label = ctk.CTkLabel(app.tabview.tab("Preview com Grid"), text="Selecione uma imagem para ver o preview", text_color=app.COLOR_TEXT)
-        app.preview_label.pack(padx=20, pady=20, expand=True, fill="both")
+        app.preview_label.pack(padx=0, pady=0, expand=True, fill="both")
 
         # Aba de Log de Atividades
         app.log_textbox = ctk.CTkTextbox(app.tabview.tab("Log de Atividades"), text_color=app.COLOR_TEXT, fg_color="transparent", activate_scrollbars=True)
-        app.log_textbox.pack(padx=20, pady=20, expand=True, fill="both")
+        app.log_textbox.pack(padx=0, pady=0, expand=True, fill="both")
 
         # Aba de Gerador de Paleta de Cores
         palette_tab_frame = ctk.CTkFrame(app.tabview.tab("Gerador de Paleta de Cores"), fg_color="transparent")
-        palette_tab_frame.pack(padx=20, pady=20, expand=True, fill="both")
+        palette_tab_frame.pack(padx=20, pady=2, expand=True, fill="both")
         palette_tab_frame.grid_rowconfigure(0, weight=0)  # Preview
         palette_tab_frame.grid_rowconfigure(1, weight=1)  # Lista de cores
         palette_tab_frame.grid_columnconfigure(0, weight=1)
 
         # Preview da imagem na aba de paleta
-        app.palette_preview_label = ctk.CTkLabel(palette_tab_frame, text="Preview da imagem", text_color=app.COLOR_TEXT)
-        app.palette_preview_label.grid(row=0, column=0, sticky="nsew", padx=20, pady=(0, 10))
+        app.palette_preview_label = ctk.CTkLabel(palette_tab_frame, text="Selecione uma imagem para ver o preview", text_color=app.COLOR_TEXT)
+        app.palette_preview_label.grid(row=0, column=0, sticky="nsew", padx=20, pady=(2))
 
         # Frame para a paleta de cores
         app.palette_frame = ctk.CTkFrame(palette_tab_frame, fg_color="transparent")
-        app.palette_frame.grid(row=1, column=0, sticky="ew", padx=20, pady=20)
+        app.palette_frame.grid(row=1, column=0, sticky="ew", padx=20, pady=2)
         app.palette_frame.grid_columnconfigure(0, weight=1)
         app.palette_frame.grid_rowconfigure(0, weight=1)
 
